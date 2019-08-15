57975e0c83541e8f89234e485bd24b7849c5e710
Douwe Bos <d.bos@hotmail.com>
2019-08-15 13:03:46 +0300

    stupid stackoverflow post...

--- DJBExtensionKit/Classes/UIColor.swift
+++ DJBExtensionKit/Classes/UIColor.swift
@@ -18,11 +18,11 @@ public extension UIColor {
     ///   - greenByte: 0-255 value for green
     ///   - blueByte: 0-255 value for blue
     ///   - alpha: 0.0-1.0 value for alpha
-    convenience init(red: Int = 0, green: Int = 0, blue: Int = 0, opacity: Int = 255) {
+    convenience init(red: Int = 0, green: Int = 0, blue: Int = 0, opacity: CGFloat = 1.0) {
         precondition(0...255 ~= red   &&
             0...255 ~= green &&
             0...255 ~= blue  &&
             0...255 ~= opacity, "input range is out of range 0...255")
-        self.init(red: CGFloat(red)/255, green: CGFloat(green)/255, blue: CGFloat(blue)/255, alpha: CGFloat(opacity)/255)
+        self.init(red: CGFloat(red)/255.0, green: CGFloat(green)/255.0, blue: CGFloat(blue)/255.0, alpha: opacity)
     }
 }
