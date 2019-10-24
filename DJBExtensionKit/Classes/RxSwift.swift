6ff3f72d02bf813852945620a192309539e013d5
Douwe Bos <d.bos@hotmail.com>
2019-10-24 22:17:15 +0200

    Make it stop complaining about E

--- DJBExtensionKit/Classes/RxSwift.swift
+++ DJBExtensionKit/Classes/RxSwift.swift
@@ -10,9 +10,9 @@
 import RxSwift
 
 public extension ObservableType {
-    func currentAndPrevious() -> Observable<(current: Element, previous: Element?)> {
-        return self.multicast({ () -> PublishSubject<Element> in PublishSubject<Element>() }) { (values: Observable<Element>) -> Observable<(current: Element, previous: Element?)> in
-            let pastValues = values.asObservable().map { previous -> Element? in previous }.startWith(nil)
+    func currentAndPrevious() -> Observable<(current: Self.E, previous: Self.E?)> {
+        return self.multicast({ () -> PublishSubject<Self.E> in PublishSubject<Self.E>() }) { (values: Observable<Self.E>) -> Observable<(current: Self.E, previous: Self.E?)> in
+            let pastValues = values.asObservable().map { previous -> Self.E? in previous }.startWith(nil)
             return Observable.zip(values.asObservable(), pastValues) { (current, previous) in
                 return (current: current, previous: previous)
             }
