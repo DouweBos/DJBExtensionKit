c0603ad032c86a01f44a1e41e992f22722476c96
Douwe Bos <d.bos@hotmail.com>
2019-10-24 22:16:59 +0200

    add capitalizedFirstLetter functio to string

--- DJBExtensionKit/Classes/String.swift
+++ DJBExtensionKit/Classes/String.swift
@@ -119,4 +119,12 @@ public extension String {
             self.replaceSubrange(r, with: with)
         }
     }
+    
+    func capitalizingFirstLetter() -> String {
+        return prefix(1).uppercased() + self.lowercased().dropFirst()
+    }
+    
+    mutating func capitalizeFirstLetter() {
+        self = self.capitalizingFirstLetter()
+    }
 }
