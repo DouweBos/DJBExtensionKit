f16d550308b4ad434b753ad066fff72ad7292784
Douwe Bos <d.bos@hotmail.com>
2019-11-08 00:19:14 +0100

    add the global KingfisherManager retrieve image with multiple sources

--- DJBExtensionKit/Classes/Kingfisher.swift
+++ DJBExtensionKit/Classes/Kingfisher.swift
@@ -19,51 +19,8 @@ public extension KingfisherWrapper where Base: KFCrossPlatformImageView {
 
         DispatchQueue.main.async {
             self.base.image = placeholder ?? self.base.image
-        }
         
-        DispatchQueue.global(qos: .userInitiated).async {
-            guard let resource = resource else { return }
-            
-            //Cause of cell reusability and the images loading async I had to add an imageFailCheck code block to the setImage function
-            //ImageFailCheck has to return if an image is still allowed to be set
-            //So as an example: make a cell compare it's local rgID variable to the rgID the cell had when it started the async setImage call
-            //If the rgIDs don't match the cell has been reused by now and the image should no longer be set
-            KingfisherManager.shared.retrieveImage(with: resource,
-                                                   options: options,
-                                                   progressBlock: progressBlock) { (result) in
-                                                    switch result {
-                                                    case .success(let imageResult):
-                                                        DispatchQueue.main.async {
-                                                            if imageFailCheck() {
-                                                                self.base.image = imageResult.image
-                                                                
-                                                                completionHandler?(result)
-                                                            } else {
-                                                                //print("Did fail image check: \(imageURL?.absoluteString ?? "No URL")")
-                                                            }
-                                                        }
-                                                    case .failure:
-                                                        DispatchQueue.main.async {
-                                                            completionHandler?(result)
-                                                        }
-                                                    }
-            }
-        }
-    }
-    
-    func setImage(with resources: [Resource?]?,
-                         placeholder: UIImage? = nil,
-                         options: KingfisherOptionsInfo? = nil,
-                         progressBlock: DownloadProgressBlock? = nil,
-                         imageFailCheck: @escaping (() -> Bool) = { return true },
-                         completionHandler: ((Result<RetrieveImageResult, KingfisherError>) -> Void)? = nil) {
-
-        DispatchQueue.main.async {
-            self.base.image = placeholder ?? self.base.image
-        }
-        
-        DispatchQueue.global(qos: .userInitiated).async {
-            if let resources = resources, let resource = resources.first {
+            DispatchQueue.global(qos: .userInitiated).async {
                 guard let resource = resource else { return }
                 
                 //Cause of cell reusability and the images loading async I had to add an imageFailCheck code block to the setImage function
@@ -81,22 +38,97 @@ public extension KingfisherWrapper where Base: KFCrossPlatformImageView {
                                                                     
                                                                     completionHandler?(result)
                                                                 } else {
-                                                                    //print("Did fail image check: \(imageURL?.absoluteString ?? "No URL")")
+                                                                    print("Did fail image check: \(resource)")
                                                                 }
                                                             }
                                                         case .failure:
                                                             DispatchQueue.main.async {
-                                                                self.setImage(with: Array(resources.dropFirst()),
-                                                                              placeholder: placeholder,
-                                                                              options: options,
-                                                                              progressBlock: progressBlock,
-                                                                              imageFailCheck: imageFailCheck,
-                                                                              completionHandler: completionHandler)
+                                                                completionHandler?(result)
                                                             }
                                                         }
                 }
             }
         }
     }
+    
+    func setImage(with resources: [Resource?]?,
+                  placeholder: UIImage? = nil,
+                  options: KingfisherOptionsInfo? = nil,
+                  progressBlock: DownloadProgressBlock? = nil,
+                  imageFailCheck: @escaping (() -> Bool) = { return true },
+                  completionHandler: ((Result<RetrieveImageResult, KingfisherError>) -> Void)? = nil) {
+
+        DispatchQueue.main.async {
+            self.base.image = placeholder ?? self.base.image
+        
+            DispatchQueue.global(qos: .userInitiated).async {
+                if let resources = resources, let resource = resources.first {
+                    guard let resource = resource else { return }
+                    
+                    //Cause of cell reusability and the images loading async I had to add an imageFailCheck code block to the setImage function
+                    //ImageFailCheck has to return if an image is still allowed to be set
+                    //So as an example: make a cell compare it's local rgID variable to the rgID the cell had when it started the async setImage call
+                    //If the rgIDs don't match the cell has been reused by now and the image should no longer be set
+                    KingfisherManager.shared.retrieveImage(with: resource,
+                                                           options: options,
+                                                           progressBlock: progressBlock) { (result) in
+                                                            switch result {
+                                                            case .success(let imageResult):
+                                                                DispatchQueue.main.async {
+                                                                    if imageFailCheck() {
+                                                                        self.base.image = imageResult.image
+                                                                        
+                                                                        completionHandler?(result)
+                                                                    } else {
+                                                                        print("Did fail image check: \(resource)")
+                                                                    }
+                                                                }
+                                                            case .failure:
+                                                                DispatchQueue.main.async {
+                                                                    self.setImage(with: Array(resources.dropFirst()),
+                                                                                  placeholder: placeholder,
+                                                                                  options: options,
+                                                                                  progressBlock: progressBlock,
+                                                                                  imageFailCheck: imageFailCheck,
+                                                                                  completionHandler: completionHandler)
+                                                                }
+                                                            }
+                    }
+                }
+            }
+        }
+    }
 }
+
+public extension KingfisherManager {
+    func retrieveImage(with resources: [Resource],
+                       options: KingfisherOptionsInfo? = nil,
+                       progressBlock: DownloadProgressBlock? = nil,
+                       imageFailCheck: @escaping (() -> Bool) = { return true },
+                       completionHandler: ((Result<RetrieveImageResult, KingfisherError>) -> Void)? = nil) {
+        if let resource = resources.first {
+            retrieveImage(with: resource, options: options, progressBlock: progressBlock) { (result) in
+                switch result {
+                case .success:
+                    DispatchQueue.main.async {
+                        if imageFailCheck() {
+                            completionHandler?(result)
+                        } else {
+                            print("Did fail image check: \(resource)")
+                        }
+                    }
+                case .failure:
+                    DispatchQueue.main.async {
+                        self.retrieveImage(with: Array(resources.dropFirst()),
+                                           options: options,
+                                           progressBlock: progressBlock,
+                                           imageFailCheck: imageFailCheck,
+                                           completionHandler: completionHandler)
+                    }
+                }
+            }
+        }
+    }
+}
+
 #endif
