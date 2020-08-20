ef66ab01e867dba923f1bc3c7fe1290742bbe47d
Douwe Bos <douwe@douwebos.nl>
2020-08-20 18:07:33 +0100

    something something and pointer control support to uicontrol

---
+++ DJBExtensionKit/Classes/UIControl.swift
@@ -0,0 +1,24 @@
+//
+//  UIControl.swift
+//  DJBExtensionKit
+//
+//  Created by Douwe Bos on 20/8/20.
+//
+
+import UIKit
+
+@available(iOS 13.4, *)
+extension UIControl: UIPointerInteractionDelegate {
+    fileprivate func enablePointerInteraction() {
+        self.addInteraction(UIPointerInteraction(delegate: self))
+    }
+
+    // This isn't even needed — just indicating what the default does!
+    public func pointerInteraction(_ interaction: UIPointerInteraction, regionFor request: UIPointerRegionRequest, defaultRegion: UIPointerRegion) -> UIPointerRegion? {
+        return defaultRegion
+    }
+
+    public func pointerInteraction(_ interaction: UIPointerInteraction, styleFor region: UIPointerRegion) -> UIPointerStyle? {
+        return UIPointerStyle(effect: .automatic(.init(view: self)))
+    }
+}
