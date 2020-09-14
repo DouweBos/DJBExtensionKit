3a54aba03661927440448ced4364715e4df072cb
Douwe Bos <douwe@douwebos.nl>
2020-09-14 22:38:53 +0100

    fix

--- DJBExtensionKit/Classes/String.swift
+++ DJBExtensionKit/Classes/String.swift
@@ -62,7 +62,7 @@ public extension String {
     ///
     /// - Returns: URLENcoded `self`
     func encodeUrl() -> String {
-        return self.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) ?? self
+        return self.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? self
     }
     
     
