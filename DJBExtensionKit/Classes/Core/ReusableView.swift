718e587c4494643d771f29773f22057eab49131b
Douwe Bos <douwe@douwebos.nl>
2021-09-29 23:15:36 +0200

    fix failing build
    Signed-off-by: Douwe Bos <douwe@douwebos.nl>

--- DJBExtensionKit/Classes/Core/ReusableView.swift
+++ DJBExtensionKit/Classes/Core/ReusableView.swift
@@ -7,15 +7,7 @@
 
 import Foundation
 import UIKit
-import RxSwift
 
 @objc public protocol ReusableView: AnyObject {
     func prepareForReuse()
 }
-
-//public extension Reactive where Base: UIView, Base: ReusableView {
-//    public var prepareForReuse: Observable<Void> {
-//        return methodInvoked(#selector(ReusableView.prepareForReuse))
-//            .map { _ in return }
-//    }
-//}
