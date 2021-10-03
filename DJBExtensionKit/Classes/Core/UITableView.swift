7b6cf48128b4006182cb1d4f7375769b3f7269f7
Douwe Bos <douwe@douwebos.nl>
2021-10-04 00:46:44 +0200

    deleting broken code
    Signed-off-by: Douwe Bos <douwe@douwebos.nl>

--- DJBExtensionKit/Classes/Core/UITableView.swift
+++ DJBExtensionKit/Classes/Core/UITableView.swift
@@ -9,20 +9,16 @@
 import UIKit
 
 public extension UITableView {
-    func register<T: UITableViewCell>(cell: T.Type) {
-        register(cell.nib, forCellReuseIdentifier: cell.reuseIdentifier)
-    }
-}
-
-public extension UITableView {
     func animateChanges(duration: TimeInterval, completion: ((Bool) -> Swift.Void)? = nil) {
-        UIView.animate(withDuration: duration,
-                       delay: 0.0,
-                       options: .curveEaseInOut,
-                       animations: { [weak self] in
-                        self?.beginUpdates()
-                        self?.endUpdates()
-                       },
-                       completion: completion)
+        UIView.animate(
+            withDuration: duration,
+            delay: 0.0,
+            options: .curveEaseInOut,
+            animations: { [weak self] in
+                self?.beginUpdates()
+                self?.endUpdates()
+            },
+            completion: completion
+        )
     }
 }
